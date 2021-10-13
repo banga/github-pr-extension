c6d3f796c7a8c871eb91ee476344e77936155eba
Shrey Banga <banga.shrey@gmail.com>
2021-10-13 11:34:29 -0700

    Make it work for PRs

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-A Chrome and Firefox extension that adds keyboard shortcuts for navigating PRs
+A Chrome and Firefox extension that adds keyboard shortcuts for navigating PRs:
+
+* `j` and `k` to jump to the next/previous change
+* `n` and `p` to jump to the next/previous comment
+* `l` to like the focused comment
+* `r` to reply to the focused comment
--- chrome/src/main.js
+++ chrome/src/main.js
@@ -1,64 +1,115 @@
-const INLINE_COMMENT_CLASS = "review-comment";
+const INLINE_COMMENT_CLASS = 'review-comment';
 const INLINE_COMMENT_SELECTOR = `#files tr.inline-comments .js-comments-holder > .${INLINE_COMMENT_CLASS}`;
-const GLOBAL_COMMENT_CLASS = "timeline-comment";
+const GLOBAL_COMMENT_CLASS = 'timeline-comment';
 const GLOBAL_COMMENT_SELECTOR = `#comments .${GLOBAL_COMMENT_CLASS}`;
-const COMMENT_FOCUSED_CLASS = "focused-by-extension";
+const ALL_COMMENTS_SELECTOR = `${INLINE_COMMENT_SELECTOR},${GLOBAL_COMMENT_SELECTOR}`;
+const FOCUSED_CLASS = 'focused-by-extension';
 const LIKE_BUTTON_SELECTOR = "button[data-reaction-label='+1']";
-const GLOBAL_REPLY_SELECTOR = "#all_commit_comments .timeline-new-comment button.write-tab";
-const INLINE_REPLY_SELECTOR = ".review-thread-reply-button";
+const GLOBAL_REPLY_SELECTOR = '#all_commit_comments .timeline-new-comment button.write-tab';
+const INLINE_REPLY_SELECTOR = '.review-thread-reply-button';
+const ADD_LINE_COMMENT_BUTTON_SELECTOR =
+    '.blob-code-addition > .add-line-comment, .blob-code-deletion > .add-line-comment';
 
+/** @return {Array<Element>}  */
 function query(selector) {
-    return document.querySelectorAll(selector);
+    return Array.from(document.querySelectorAll(selector));
 }
 
-function getAll() {
-    return query(`${INLINE_COMMENT_SELECTOR},${GLOBAL_COMMENT_SELECTOR}`);
+/* Focus */
+
+function focusElement(elem) {
+    getFocusedElement().forEach((comment) => comment.classList.remove(FOCUSED_CLASS));
+    elem.classList.add(FOCUSED_CLASS);
+    elem.focus();
+    elem.scrollIntoViewIfNeeded();
 }
 
-function getFocused() {
-    return query(`.${COMMENT_FOCUSED_CLASS}`);
+function getFocusedElement() {
+    return query(`.${FOCUSED_CLASS}`);
 }
 
+/** @param {Array<Element>} elems  */
 function getFocusedIndex(elems) {
-    for (var i = 0; i < elems.length; i++) {
-        if (elems[i].classList.contains(COMMENT_FOCUSED_CLASS)) {
-            return i;
-        }
-    }
-    return -1;
+    return elems.findIndex((elem) => elem.classList.contains(FOCUSED_CLASS));
 }
 
-function focusNext() {
-    const all = getAll();
-    if (!all) {
-        return;
-    }
-    const focusedIndex = getFocusedIndex(all);
-    if (focusedIndex < all.length - 1) {
-        focus(all[focusedIndex + 1]);
+function focusNext(items) {
+    const focusedIndex = getFocusedIndex(items);
+    if (focusedIndex < items.length - 1) {
+        focusElement(items[focusedIndex + 1]);
     }
 }
 
-function focusPrevious() {
-    const all = getAll();
-    if (!all) {
-        return;
-    }
-    const focusedIndex = getFocusedIndex(all);
+function focusPrevious(items) {
+    const focusedIndex = getFocusedIndex(items);
     if (focusedIndex > 0) {
-        focus(all[focusedIndex - 1]);
+        focusElement(items[focusedIndex - 1]);
     } else {
-        focus(all[0]);
+        focusElement(items[0]);
     }
 }
+
+/* Comments */
+
+function getAllComments() {
+    return query(ALL_COMMENTS_SELECTOR);
+}
+
+function focusNextComment() {
+    focusNext(getAllComments());
+}
+
+function focusPreviousComment() {
+    focusPrevious(getAllComments());
+}
+
+/* Changes */
+
+function getParentRowForAddCommentButton(element) {
+    return element.parentElement.parentElement;
+}
+
+function getCommentButtonsAtChangeBoundaries() {
+    const elements = query(ADD_LINE_COMMENT_BUTTON_SELECTOR);
+    if (elements.length === 0) {
+        return [];
+    }
+
+    // Filter out buttons in rows that are not at the end of a contiguous change
+    const filteredElements = [];
+    let currentElement = elements[0];
+    for (const nextElement of elements.slice(1)) {
+        if (
+            getParentRowForAddCommentButton(currentElement) !==
+                getParentRowForAddCommentButton(nextElement) &&
+            getParentRowForAddCommentButton(currentElement).nextElementSibling !==
+                getParentRowForAddCommentButton(nextElement)
+        ) {
+            filteredElements.push(currentElement);
+        }
+        currentElement = nextElement;
+    }
+    filteredElements.push(currentElement);
+
+    return filteredElements;
+}
+
+function focusNextChange() {
+    focusNext(getCommentButtonsAtChangeBoundaries());
+}
+
+function focusPreviousChange() {
+    focusPrevious(getCommentButtonsAtChangeBoundaries());
+}
+
+/* Actions */
 
 function toggleLike() {
-    query(`.${COMMENT_FOCUSED_CLASS} ${LIKE_BUTTON_SELECTOR}`)
-        .forEach(comment => comment.click());
+    query(`.${FOCUSED_CLASS} ${LIKE_BUTTON_SELECTOR}`).forEach((comment) => comment.click());
 }
 
 function startReply() {
-    const focused = getFocused();
+    const focused = getFocusedElement();
     if (!focused.length) {
         return;
     }
@@ -70,30 +121,35 @@ function startReply() {
     }
 }
 
-function focus(elem) {
-    getFocused().forEach(
-        comment => comment.classList.remove(COMMENT_FOCUSED_CLASS));
-    elem.classList.add(COMMENT_FOCUSED_CLASS);
-    elem.scrollIntoViewIfNeeded();
-}
-
-window.addEventListener("keydown", function(e) {
-    if (e.altKey || e.shiftKey || e.ctrlKey || e.metaKey ||
-        e.target.nodeName == "TEXTAREA" || e.target.nodeName == "INPUT" ||
-        e.target.getAttribute("contentEditable") === "true") {
+window.addEventListener('keydown', function (e) {
+    if (
+        e.altKey ||
+        e.shiftKey ||
+        e.ctrlKey ||
+        e.metaKey ||
+        e.target.nodeName == 'TEXTAREA' ||
+        e.target.nodeName == 'INPUT' ||
+        e.target.getAttribute('contentEditable') === 'true'
+    ) {
         return;
     }
     switch (e.key) {
-        case "j":
-            focusNext();
+        case 'j':
+            focusNextChange();
             break;
-        case "k":
-            focusPrevious();
+        case 'k':
+            focusPreviousChange();
             break;
-        case "l":
+        case 'n':
+            focusNextComment();
+            break;
+        case 'p':
+            focusPreviousComment();
+            break;
+        case 'l':
             toggleLike();
             break;
-        case "r":
+        case 'r':
             startReply();
             e.preventDefault();
             break;
